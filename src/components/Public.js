8d64bf61a22bdc1b0820c052949d1caa9aa1a0d4
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2023-07-09 15:56:41 -0300

    xdddd

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,18 +4,18 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome to <span className="nowrap">Dan D. Repairs!</span></h1>
+                <h1>Welcome to <span className="nowrap">Eduardo Lange Repairs!</span></h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown Foo City, Dan D. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <p>Located in Beautiful Downtown Vera Cruz City, Eduardo Lange Repairs provides a trained staff ready to meet your tech repair needs.</p>
                 <address className="public__addr">
-                    Dan D. Repairs<br />
-                    555 Foo Drive<br />
-                    Foo City, CA 12345<br />
-                    <a href="tel:+15555555555">(555) 555-5555</a>
+                    Eduardo Lange Repairs<br />
+                    Rua Tiradentes 618<br />
+                    Vera Cruz, BR 12345<br />
+                    <a href="tel:+15555555555">+51 (51) 99730-8699</a>
                 </address>
                 <br />
-                <p>Owner: Dan Davidson</p>
+                <p>Owner: Eduardo Lange</p>
             </main>
             <footer>
                 <Link to="/login">Employee Login</Link>
